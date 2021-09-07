fe8105d3918bb44b56d6f5da68f294087ea6ede6
acxel <acxelandres@outlook.com>
2021-09-07 18:32:40 -0500

    ProyectoEstable

---
+++ Datos/inventario/PersonalD.cs
@@ -0,0 +1,103 @@
+﻿using Logica.inventario;
+using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+namespace Datos.inventario
+{
+    public class PersonalD
+    {
+        public bool InsertarCaja(Caja parametros)
+        {
+            try
+            {
+                CONEXIONMAESTRA.abrir();
+                SqlCommand cmd = new SqlCommand("InsertarCaja",CONEXIONMAESTRA.conectar);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@SaldoApertura", parametros.SaldoApertura1);
+                cmd.Parameters.AddWithValue("@SaldoCierre", parametros.SaldoCierre1);
+                cmd.Parameters.AddWithValue("@SaldoActual", parametros.SaldoActual1);
+                cmd.Parameters.AddWithValue("@Observacion", parametros.Observacion1);
+                cmd.Parameters.AddWithValue("@Fecha", parametros.Fecha1);
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            finally
+            {
+                CONEXIONMAESTRA.cerrar();
+            }
+        }
+        public bool CerrarCaja(Caja parametros)
+        {
+            try
+            {
+                CONEXIONMAESTRA.abrir();
+                SqlCommand cmd = new SqlCommand("CerrarCaja", CONEXIONMAESTRA.conectar);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id_Caja", parametros.IdCaja1);
+                
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            finally
+            {
+                CONEXIONMAESTRA.cerrar();
+            }
+        }
+        public bool AnularCaja(Caja parametros)
+        {
+            try
+            {
+                CONEXIONMAESTRA.abrir();
+                SqlCommand cmd = new SqlCommand("AnularCaja", CONEXIONMAESTRA.conectar);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id_Caja", parametros.IdCaja1);
+
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            finally
+            {
+                CONEXIONMAESTRA.cerrar();
+            }
+        }
+
+        public void MostrarCaja(ref DataTable dataTable)
+        {
+            try
+            {
+                CONEXIONMAESTRA.abrir();
+                SqlDataAdapter da = new SqlDataAdapter("MostrarCaja", CONEXIONMAESTRA.conectar);
+                da.Fill(dataTable);
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.StackTrace);
+            }
+        }
+    }
+}
--- SistemaLaBocana/Vistas/ControlUsuario/Caja.cs
+++ SistemaLaBocana/Vistas/ControlUsuario/Caja.cs
@@ -1,4 +1,4 @@
-﻿using Logica.inventario;
+﻿
 using SistemaLaBocana.Vistas.Panels;
 using System;
 using System.Collections.Generic;
@@ -18,110 +18,6 @@ namespace SistemaLaBocana.Vistas.ControlUsuario
         {
             InitializeComponent();
         }
-
-        CajaLN oppln = new CajaLN();
-        public DataGridView dato;
-        public Caja auxiliar;
-
-        //Muestra la caja
-        public void MostrarCaja()
-        {
-            dtgCaja.DataSource = oppln.ViewCaja();
-        }
-
-        //Agrega Nueva caja
-        public void nuevo()
-        {
-            try
-            {
-                FrmAbrirCaja fec = new FrmAbrirCaja();
-                fec.Text = "Ingresar Caja";
-                fec.opc = 1;
-                fec.ShowDialog();
-                if (fec.DialogResult == DialogResult.OK)
-                {
-                    Caja op = fec.crearObjeto();
-                    oppln.OpenCaja(op);
-                    fec.Close();
-                    MessageBox.Show("Se ha ingresado la caja...");
-                }
-                else
-                {
-                    fec.Close();
-                    MessageBox.Show("Ingreso cancelado...");
-                }
-                MostrarCaja();
-            }
-            catch (Exception ex)
-            {
-
-                MessageBox.Show("Error al insertar caja " + ex.Message);
-            }
-        }
-
-        //Anula la caja
-        public void Eliminar()
-        {
-            try
-            {
-                if (dtgCaja.CurrentCell != null)
-                {
-                    var res = MessageBox.Show("¿Esta seguro de Anular?", "Eliminar", MessageBoxButtons.YesNo);
-                    if (res.ToString().Equals("Yes"))
-                    {
-                        CajaLN loc = new CajaLN();
-                        Caja objeto = dtgCaja.CurrentRow.DataBoundItem as Caja;
-
-                        loc.DeleteCaja(objeto);
-                        MessageBox.Show("Se ha anulado la caja seleccionada");
-                    }
-                    else
-                    {
-                        MessageBox.Show("Anulacion cancelada");
-                    }
-                }
-
-                MostrarCaja();
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        //Cerrar caja
-
-        public void CerrarCaja()
-        {
-            try
-            {
-                if (dtgCaja.CurrentCell != null)
-                {
-                    var res = MessageBox.Show("¿Esta seguro de Cerrar la caja?", "Cerrar", MessageBoxButtons.YesNo);
-                    if (res.ToString().Equals("Yes"))
-                    {
-                        CajaLN loc = new CajaLN();
-                        Caja objeto = dtgCaja.CurrentRow.DataBoundItem as Caja;
-
-                        loc.CloseCaja(objeto);
-                        MessageBox.Show("Se ha cerrado la caja seleccionada");
-                    }
-                    else
-                    {
-                        MessageBox.Show("No se cerro la caja seleccionada");
-                    }
-                }
-
-                MostrarCaja();
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
         private void button5_Click(object sender, EventArgs e)
         {
             FrmAbrirCaja frm = new FrmAbrirCaja();
